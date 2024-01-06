e10eb40e55a6fd7785327f9debe5e2289fd65185
0rigin-c0de <shunnu807@gmail.com>
2024-01-06 15:45:15 +0530

    initial-commit

--- src/components /writePost/Navbar.jsx
+++ src/components /writePost/Navbar.jsx
@@ -6,34 +6,29 @@ import "../../index.css";
 const Navbar = ({ handleSubmit, response }) => {
   const { userData } = useAuthContent();
   return (
-    <div className="container mx-auto flex items-center justify-between ">
-      <div className="flex items-center gap-2 md:gap-6">
+    <div className="container mx-auto flex items-center justify-between py-4">
+      <div className="flex items-center gap-4 md:gap-6">
         <Link to="/">
-          <img src={Logo} alt="logo" className="md:w-14 w-8 cursor-pointer" />
+          <img src={Logo} alt="logo" className="md:w-12 w-8 cursor-pointer" />
         </Link>
-        <p className="md:text-xl text-md md:w-full w-28">
-          Draft in {userData.displayName}{" "}
+        <p className="md:text-lg text-sm md:w-full w-32 font-semibold">
+          Draft in {userData.displayName}
         </p>
       </div>
-      <div className="flex items-center gap-5">
-        {!response.Ispending && (
-          <button
-            onClick={handleSubmit}
-            aria-label="publish article"
-            className="button"
-          >
+      <div className="flex items-center gap-4">
+        {!response.Ispending ? (
+          <button onClick={handleSubmit} className="button">
             Publish
           </button>
-        )}
-        {response.Ispending && (
-          <button disabled aria-label="publish article" className="button ">
+        ) : (
+          <button disabled className="button opacity-50 cursor-not-allowed">
             Publishing
           </button>
         )}
         <img
           src={userData.photoURL}
           alt={userData.displayName}
-          className="md:w-11 w-4 rounded-full cursor-pointer"
+          className="md:w-10 w-6 rounded-full cursor-pointer"
         />
       </div>
     </div>
